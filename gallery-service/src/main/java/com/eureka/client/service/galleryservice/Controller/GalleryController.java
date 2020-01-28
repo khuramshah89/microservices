0d53a240b7348f4b6ca368fc89f56992c175fd3d
Khuram Shehzad Shah <kshahzad@pioneerlogics.com>
2020-01-28 18:41:30 +0500

    added dependecy for ribbon

--- gallery-service/src/main/java/com/eureka/client/service/galleryservice/Controller/GalleryController.java
+++ gallery-service/src/main/java/com/eureka/client/service/galleryservice/Controller/GalleryController.java
@@ -2,7 +2,18 @@ package com.eureka.client.service.galleryservice.Controller;
 
 import com.eureka.client.service.galleryservice.Model.Gallery;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +35,11 @@ public class GalleryController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         // This is useful for debugging
         // When having multiple instance of gallery service running at different ports.
         // We load balance among them, and display which instance received the request.
@@ -34,12 +48,17 @@ public class GalleryController {
 
 
     @GetMapping("/{id}")
-    public Gallery getGallery(@PathVariable int id){
+    public Gallery getGallery(@PathVariable int id) {
 
-        Gallery gallery=new Gallery();
+        ServiceInstance instance = loadBalancerClient.choose("image-service");
+        String baseUrl = instance.getUri().toString();
+        baseUrl = baseUrl + "/images";
+        System.out.println(baseUrl);
+        Gallery gallery = new Gallery();
         gallery.setId(id);
 
-        List<Object> images = restTemplate.getForObject("http://image-service/images", List.class);;
+        List<Object> images = restTemplate.getForObject(baseUrl, List.class);
+
         gallery.setImages(images);
 
         return gallery;
@@ -52,6 +71,13 @@ public class GalleryController {
     @RequestMapping("/admin")
     public String homeAdmin() {
         return "This is the admin area of Gallery service running at port: " + env.getProperty("local.server.port");
+    }
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
 
--- gallery-service/src/main/java/com/eureka/client/service/galleryservice/GalleryServiceApplication.java
+++ gallery-service/src/main/java/com/eureka/client/service/galleryservice/GalleryServiceApplication.java
@@ -2,6 +2,7 @@ package com.eureka.client.service.galleryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +16,6 @@ public class GalleryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GalleryServiceApplication.class, args);
-	}
-
-	@Configuration
-	class RestTemplateConfig{
-
-		@Bean
-		@Scope("singleton") //by default scope is also singleton
-		@LoadBalanced // Load balance between service instances running at different ports.
-		public RestTemplate restTemplate(){
-			return new RestTemplate();
-		}
 	}
 
 }
